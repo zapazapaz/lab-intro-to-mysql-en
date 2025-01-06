768b21e4b998b5b53aab688051dc4d04ed5edbb5
zapazapaz <113686669+zapazapaz@users.noreply.github.com>
2025-01-06 10:01:46 -0400

    Create update.sql

---
+++ your-code/update.sql
@@ -0,0 +1,18 @@
+USE cars;
+
+UPDATE salesperson
+SET store_name = 'Miami'
+WHERE salesperson_id = '00005';
+
+UPDATE costumer
+SET Email = 'ppicasso@gmail.com'
+WHERE costumer_id = '10001';
+
+UPDATE costumer
+SET Email = 'lincoln@us.gov'
+WHERE costumer_id = '20001';
+
+UPDATE costumer
+SET Email = 'hello@napoleon.me'
+WHERE costumer_id = '30001';
+
